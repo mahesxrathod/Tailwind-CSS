e5dca9ac5e8606c580464d9ed8c20e5e96d373e9
mahesxrathod <rathodmaheshhh@gmail.com>
2025-02-01 10:33:28 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Alignment from "./Component/Alignment";
+import Background from "./Component/Background";
 import Marginpadding from "./Component/marginpadding";
 
 function App() {
   return (
     <>
      {/* <Alignment /> */}
-     <Marginpadding />
+     {/* <Marginpadding /> */}
+     <Background />
     </>
   );
 }
---
+++ src/Component/Background.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Background() {
+  return (
+    <>
+      <p class="w-[40%] mx-auto font-bold text-2xl text-center p-2">
+        Background Image
+      </p>
+      <div class="bg-fixed bg-origin-border border-4 border-dashed w-[40%] p-16 mx-auto text-white font-bold text-justify bg-[url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnGkr17Qold7tCvTSZ0vKKJ6RsLW83OG0VnA&usqp=CAU)]">
+        <p class="text-center text-2xl p-5 underline decoration-4 underline-offset-3">
+          Amazon Rainforest
+        </p>
+        The Amazon rainforest, also called Amazon jungle or Amazonia, is a moist
+        broadleaf tropical rainforest in the Amazon biome that covers most of
+        the Amazon basin of South America. This basin encompasses 7,000,000 km2
+        (2,700,000 sq mi), of which 5,500,000 km2 (2,100,000 sq mi) are covered
+        by the rainforest. This region includes territory belonging to nine
+        nations and 3,344 formally acknowledged indigenous territories. The
+        majority of the forest, 60%, is in Brazil, followed by Peru with 13%,
+        Colombia with 10%, and with minor amounts in Bolivia, Ecuador, French
+        Guiana, Guyana, Suriname, and Venezuela. Four nations have "Amazonas" as
+        the name of one of their first-level administrative regions, and France
+        uses the name "Guiana Amazonian Park" for French Guiana's protected
+        rainforest area. The Amazon represents over half of Earth's remaining
+        rainforests, and comprises the largest and most biodiverse tract of
+        tropical rainforest in the world, with an estimated 390 billion
+        individual trees in about 16,000 species.
+      </div>
+    </>
+  );
+}
+
+export default Background;
