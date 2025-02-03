b48f562804de0dbd393b410c96433371821949a6
mahesxrathod <rathodmaheshhh@gmail.com>
2025-02-03 12:33:20 +0530

    init

--- src/Component/Border.jsx
+++ src/Component/Border.jsx
@@ -14,8 +14,9 @@ function Border() {
         <div className='w-[80%] mx-auto border-2 border-double border-indigo-600  text-center text-red font-bold text-2xl p-5 mb-3 mt-3'>border-dotted</div>
         <div className='w-[80%] mx-auto outline-4 outline-offset-2 bg-[gold] border-indigo-600  text-center text-red font-bold text-2xl p-5 mb-3 mt-3'>Outline Border</div>
         <div className='w-[80%] mx-auto outline-4 outline-offset-2 outline-dashed bg-[gold] border-indigo-600  text-center text-red font-bold text-2xl p-5 mb-3 mt-3'>Outline dashed</div>
-        <div className='w-[80%] mx-auto outline-2 outline-offset-2 bg-[gold] border-indigo-600  text-center text-red font-bold text-2xl p-5 mb-3 mt-3'>Outline offset</div>
-        <div className='w-[80%] mx-auto outline-4 outline-offset-2 bg-[gold] border-indigo-600  text-center text-red font-bold text-2xl p-5 mb-3 mt-3'>Outline offset</div>
+        <div className='w-[80%] mx-auto outline-0 outline-offset-2 bg-[gold] border-indigo-600  text-center text-red font-bold text-2xl p-5 mb-3 mt-3'>Outline-0 offset</div>
+        <div className='w-[80%] mx-auto outline-4 outline-offset-2 bg-[gold] border-indigo-600  text-center text-red font-bold text-2xl p-5 mb-3 mt-3'>Outline-4 offset</div>
+        <div className='w-[80%] mx-auto outline-8 outline-offset-2 bg-[gold] border-indigo-600  text-center text-red font-bold text-2xl p-5 mb-3 mt-3'>Outline-8 offset</div>
     </div>
     </>
   )
